f88fd0b55e78a832bd25b6fd63eb2c69c6e7bb56
biagio <biagio@192.168.1.101>
2016-10-16 20:37:32 +0200

    Testing class for the Array implementations

---
+++ JavaDataStructures/src/com/biagio/dataStructures/testing/ArraysMain.java
@@ -0,0 +1,18 @@
+package com.biagio.dataStructures.testing;
+
+/**
+ * The testing class for the Array implementations
+ * @author biagio
+ */
+public class ArraysMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Test Array");
+		
+	}
+
+}
