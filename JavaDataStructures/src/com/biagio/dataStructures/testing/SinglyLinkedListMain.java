7d48ccfe3ed796b256de196105d27af697f6b38c
biagio <biagio@192.168.1.101>
2016-10-23 15:27:08 +0200

    Testing class for the Singly Linked List

---
+++ JavaDataStructures/src/com/biagio/dataStructures/testing/SinglyLinkedListMain.java
@@ -0,0 +1,38 @@
+package com.biagio.dataStructures.testing;
+
+import javax.tools.ToolProvider;
+
+import com.biagio.dataStructures.LinkedList.SinglyLinkedList;
+import com.biagio.dataStructures.LinkedList.SinglyLinkedNode;
+
+public class SinglyLinkedListMain {
+
+	public static void main(String[] args) {
+		
+		SinglyLinkedList<Integer> list=new SinglyLinkedList<Integer>();
+		list.topBack();
+		list.pushFront(3);
+		list.pushBack(4);
+		list.pushFront(2);
+		list.pushFront(5);
+		list.pushFront(6);
+		System.out.println("size: "+list.getSize());
+		System.out.println(list.toString());
+		
+		list.erase(new SinglyLinkedNode<>(6));
+		System.out.println(list.toString());
+		
+		System.out.println(list.topFront());
+		System.out.println(list.topBack());
+		
+		list.popBack();
+		System.out.println(list.toString());
+		list.popFront();
+		System.out.println(list.toString());
+		System.out.println("size: "+list.getSize());
+		System.out.println("Empty list? "+list.isEmpty());
+		
+		System.out.println("Element found? "+list.find(3));
+	}
+
+}
